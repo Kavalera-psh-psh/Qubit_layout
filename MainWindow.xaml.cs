3b278bc70b260c5756711ece3f02ba068a939d76
Kavalera-psh-psh <kioshira312002@mail.ru>
2022-12-14 19:56:02 +0300

    layout

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TunNik_Qubit
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOverview_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnExperiences_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSegments_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnVisitorCloud_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnImages_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnInsights_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnMore_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnPreview_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSaveChanges_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnRemove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnMoreTwo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnHah_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
